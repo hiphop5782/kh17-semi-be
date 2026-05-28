09b10a24a7579946b5103135cbb74d8c6748dfdf
tookwak5-sudo <tookwak5@gmail.com>
2026-05-28 14:33:00 +0900

    dd

--- semprj/src/main/java/com/kh/semiprjRestController/RlaskdudRestController.java
+++ semprj/src/main/java/com/kh/semiprjRestController/RlaskdudRestController.java
@@ -1,12 +1,12 @@
 package com.kh.semiprjRestController;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 public class RlaskdudRestController {
-	@Controller
-	public class HomeController {
-		@RequestMapping("/")
+	@RestController
+	@RequestMapping("/")
+	public class rlaskdudRestController {
 		public String home() {
 			return "rlaskdud";
 		}
