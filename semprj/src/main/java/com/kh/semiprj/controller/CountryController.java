c610d81d666ec302d22e1fc0ce119a687fcfead6
user <user@user1>
2026-05-28 15:08:02 +0900

    countryDto

---
+++ semprj/src/main/java/com/kh/semiprj/controller/CountryController.java
@@ -0,0 +1,12 @@
+package com.kh.semiprj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CountryController {
+	@RequestMapping("/country")
+	public String country() {
+		return "country";
+	}
+}
