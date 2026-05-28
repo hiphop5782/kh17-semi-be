a6d70ba521530a2e00c1d1fcb775b6ab144f4d03
hiphop5782 <zdlsz@hanmail.net>
2026-05-28 11:38:51 +0900

    최초 프로젝트 업로드

---
+++ semprj/src/main/java/com/kh/semiprj/SemprjApplication.java
@@ -0,0 +1,13 @@
+package com.kh.semiprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemprjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemprjApplication.class, args);
+	}
+
+}
---
+++ semprj/src/test/java/com/kh/semiprj/SemprjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.semiprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemprjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
